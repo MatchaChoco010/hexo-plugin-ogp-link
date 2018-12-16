84cf0935e7fb5aae411a6bd28030b0fc7a48032b
Orito Itsuki <20170107+MatchaChoco010@users.noreply.github.com>
2018-12-16 21:27:29 +0900

    Truncate string

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const ogs = require('open-graph-scraper')
+const util = require('hexo-util')
 
 hexo.extend.filter.register('before_post_render', async data => {
   let content = data.content
@@ -21,9 +22,9 @@ hexo.extend.filter.register('before_post_render', async data => {
         return `\n<a href="${data.ogUrl ||
           url}" class="hexo-plugin-ogp-link"><img src=${
           data.ogImage.url
-        }><div><h1>${data.ogTitle}</h1><div>{% raw %}${
+        }><div><h1>${data.ogTitle}</h1><div>{% raw %}${util.truncate(
           data.ogDescription
-        }{% endraw %}</div></div></a>\n`
+        )}{% endraw %}</div></div></a>\n`
       } catch (e) {
         return `\n<a href="${url}">${title}</a>\n`
       }
